cb41205b4b96aea3f1853ed23e3b31956646d0cd
mwarr-dev <56823304+mwarr-dev@users.noreply.github.com>
2020-08-12 09:32:45 -0500

    Update create_layers.py

--- create_layers.py
+++ create_layers.py
@@ -4,15 +4,11 @@ import arcpy
 # Set the workspace
 arcpy.env.workspace = 'F:\MWARREN\_scratch\_scratch.gdb'
 
-# Select all cities which overlap the chihuahua polygon
-chihuahua_cities = arcpy.SelectLayerByLocation_management('cities', 'INTERSECT', 
-                                                          'chihuahua', 0, 
-                                                          'NEW_SELECTION')
+#make layer from feature class
+arcpy.MakeFeatureLayer_management("parcels", "lyr") 
 
-# Within selected features, further select only those cities which have a 
-# population > 10,000   
-arcpy.SelectLayerByAttribute_management(chihuahua_cities, 'SUBSET_SELECTION', 
-                                        '"population" > 10000')
+# Select all cities which overlap the chihuahua polygon
+where_clause = arcpy.SelectLayerByLocation_management('lyr', 'NEW_SELECTION', " [PROP_ID] = '18277' ")
 
 # Write the selected features to a new featureclass
-arcpy.CopyFeatures_management(chihuahua_cities, 'chihuahua_10000plus')
+arcpy.CopyFeatures_management("lyr", "18277protest_test")
