652670c617110de0ade24b29df9a650e2ff9ea10
mwarr-dev <56823304+mwarr-dev@users.noreply.github.com>
2020-08-27 14:43:55 -0500

    Update create_layers.py

--- create_layers.py
+++ create_layers.py
@@ -11,6 +11,7 @@ arcpy.env.workspace = r'F:\MWARREN\_scratch\_scratch.gdb'
 
 PROP_ID = input("Enter PROP_ID: ")
 where_clause ='"PROP_ID" = ' + '{}'.format(PROP_ID)
+distance = "200 Feet"
 
 # Create selection of parcel with indicated input
 parcel = arcpy.SelectLayerByAttribute_management("ownership", "NEW_SELECTION", where_clause)
@@ -19,7 +20,7 @@ parcel = arcpy.SelectLayerByAttribute_management("ownership", "NEW_SELECTION", w
 arcpy.CopyFeatures_management(parcel, "parcel{}".format(PROP_ID))
 
 # Create selection of 200ft buffer around indicated parcel
-buffer200 = arcpy.SelectLayerByLocation_management("ownership", 'INTERSECT', 'parcel{}'.format(PROP_ID), "200 Feet", 'NEW_SELECTION')
+buffer200 = arcpy.SelectLayerByLocation_management("ownership", 'INTERSECT', 'parcel{}'.format(PROP_ID), distance, 'NEW_SELECTION')
 
 # Write the selected features to a new featureclass
 arcpy.CopyFeatures_management(buffer200, "parcel{}_200ftbuffer".format(PROP_ID))
